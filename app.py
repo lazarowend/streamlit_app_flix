c10bb73b36373fe0caabde883e029a2a2e9d577c
lazarowend <lazaroclans1@gmail.com>
2024-11-13 10:42:38 -0300

    options add

--- app.py
+++ app.py
@@ -4,8 +4,26 @@ import streamlit as st
 def main():
     st.title('Flix app')
 
-
-
+    menu_option = st.sidebar.selectbox(
+        'Selecione uma opção',
+        ['Início', 'Gêneros', 'Atores/Atrizes', 'Filmes', 'Avaliações']
+    )
+    
+    if menu_option == 'Início':
+        st.write('Início')
+        
+    if menu_option == 'Gêneros':
+        st.write('Gêneros')
+    
+    if menu_option == 'Atores/Atrizes':
+        st.write('Atores/Atrizes')
+    
+    if menu_option == 'Filmes':
+        st.write('Filmes')
+        
+    if menu_option == 'Avaliações':
+        st.write('Avaliações')
+    
 
 if __name__ == '__main__':
     main()
